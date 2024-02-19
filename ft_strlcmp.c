33d3d25fd10879c22436ee3ee180c308edb025e4
Tu Nombre <vr.rodrigo98@gmail.com>
2024-02-19 20:57:44 +0100

    Fixed memory leak

--- ft_strlcmp.c
+++ ft_strlcmp.c
@@ -1,30 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   ft_strlcmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rovargas < rovargas@student.42madrid.com > +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/23 22:24:11 by rovargas          #+#    #+#             */
-/*   Updated: 2024/02/19 20:56:17 by rovargas         ###   ########.fr       */
+/*   Created: 2024/02/19 20:56:06 by rovargas          #+#    #+#             */
+/*   Updated: 2024/02/19 20:56:27 by rovargas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void *ft_memchr(const void *s, int c, size_t n)
-{
-    char *str;
-
-    str = (char*)s;
-    if(n)
-        {
-            while(*str++)
-            {
-                if(*str == c)
-                    return(str);
-            }
-            return(NULL);
-        }
-    else
-    return(NULL);
-
-}
+terminal
